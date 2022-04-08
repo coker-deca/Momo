2bdba69fd6a47cbd9c9d731db3cf6e03036eb192
tosincoker26 <tosincoker26@gmail.com>
2022-04-08 17:57:42 +0100

    Initial Components for contents

---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'react';
+
+const SignIn: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/components/SignUp/SignUp.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'react';
+
+const SignUp: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+    </div>
+  );
+};
+
+export default SignUp;
